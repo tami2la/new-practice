e8328d268312be7d66369db358dbbc4f51889749
Tami2la <tami.padilla12@outlook.com>
2021-08-21 19:15:21 -0300

    proyect

--- js/login.js
+++ js/login.js
@@ -2,17 +2,5 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-document.getElementById("log").addEventListener("click", login);
 
 });
- function login(){
- let usuario = document.getElementById('usuario').value;
- let contraseña = document.getElementById('contraseña').value;
- if((usuario != "") && (contraseña != "")) {
-    window.location.href="home.html"
-} else {
-   alert("Hay campos vacios, completar para iniciar sesion");
- }
-
-}
- 
