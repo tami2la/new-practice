1c8a3e1975164ce92c31fbaac3d963f6e0eb8e6f
Tamara Padilla <88443979+tami2la@users.noreply.github.com>
2021-09-19 22:55:06 -0300

    Add files via upload

--- product-info.js
+++ product-info.js
@@ -1,10 +1,8 @@
-
-//nuevo codigo
-
-
+//Creamos una variable de producto
 var product = {};
-var comments = {};
-function showImages(array) {
+
+//Funcion para desplegar todas las imagenes del producto
+function mostrarImagenes(array) {
 
     let htmlContentToAppend = "";
 
@@ -12,25 +10,27 @@ function showImages(array) {
         let images = array[i];
 
         htmlContentToAppend += `
-        <a href="` + images + `"
-            <div class="col-lg-3 col-md-4 col-6">
-                <div class="d-block mb-4 h-100">
-                    <img class="img-fluid img-thumbnail" src="` + images + `" alt="">
+            <a href="` + images + `"
+                <div class="col-lg-3 col-md-4 col-6">
+                    <div class="d-block mb-4 h-100">
+                        <img class="img-fluid img-thumbnail" src="` + images + `" alt="">
+                    </div>
                 </div>
-            </div>
-        </a>
-        `
+            </a>
+            `
 
         document.getElementById("product-images").innerHTML = htmlContentToAppend;
     }
 }
-function showComments(array) {
+
+//Funcion para mostrar la seccion de comentarios y su respectiva valoracion
+function mostrarComentarios(array) {
 
     let htmlContentToAppend = "";
 
     for (let i = 0; i < array.length; i++) {
         let comments = array[i];
-
+        
         htmlContentToAppend += `
         
         <div class="list-group-item list-group-item-action">
@@ -45,82 +45,116 @@ function showComments(array) {
         </div>
         
         `
-
+        //Guardamos la informacion en el html
         document.getElementById("comments-section").innerHTML = htmlContentToAppend;
     }
 }
+
+//Activamos la funcion JSON para mostrar la informacion completa del producto obtenida del servidor
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            product = resultObj.data;
+//Creamos las variables que contienen la informacion
+            let nombreProducto= document.getElementById("product-name");
+            let descripcionProducto = document.getElementById("product-description");
+            let cantidadProducto = document.getElementById("product-count");
+           
+//Agregamos la informacion de las variables como items de la variable producto
+            nombreProducto.innerHTML = product.name;
+            descripcionProducto.innerHTML = product.description;
+            cantidadProducto.innerHTML = product.soldCount;
+            
+
+            //Muestro las imagenes en forma de galería
+            mostrarImagenes(product.images);
+        }
+    });
+});
+//Activo la funcion JSON para obtener la informacion de los comentarios del servidor.
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            comments = resultObj.data;
+
+            mostrarComentarios(comments);
+            commentario()
+        }
+    });
+});
+
+//Activo el escuchador de eventos para que al hacer click se active la funcion
+//que agrega comentarios con su respectivo nombre de usuario y su valoracion en estrellas.
+document.getElementById("enviar").addEventListener("click", commentario)
+    function commentario(){
+            let user = sessionStorage.getItem("Usuario");
+            let agregarComentario = document.getElementById("comment").value;
+            let valoracion = agregarEstrellas(document.getElementsByName("estrellas"));
+          //Obtenemos la fecha y hora actual y la guardamos en variables
+            let hoy = new Date();
+            let dia = hoy.getDate();
+            let mes = hoy.getMonth() +1 ;
+            let anio = hoy.getFullYear();
+            let hora = hoy.getHours();
+            let minutos = hoy.getMinutes();
+            let segundos = hoy.getSeconds();
+            let fechaActual = `${anio}-${mes}-${dia} ${hora}:${minutos}:${segundos}`;
+             
+           
+            let htmlcommentToAppend = "";
+                if (agregarComentario!= ""){    
+            
+                    htmlcommentToAppend = `
+                    <div class="list-group-item list-group-item-action">
+                        <div class="row">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">` + user + `</h5><p style="text-align: right"> ` + agregarComentario + `</p> 
+                                <p> ` + mostrarEstrellas(valoracion) + `</p>  
+                            </div>
+                            <p class="mb-1"> ` + fechaActual + `</p>
+
+                        </div>
+                    </div>
+
+                    `
+                 //Agregamos la informacion al html
+                document.getElementById("comments-section").innerHTML += htmlcommentToAppend;
+        }
+    };
+
+//Funcion para mostrar las estrellas segun la valoracion de cada comentario
 function mostrarEstrellas(puntaje) {
-    let estrellaLlena = '<span class="fa fa-star"checked> </span>'
     let estrellaVacia = '<span class="fa fa-star>"</span>'
+    let estrellaLlena = '<span class="fa fa-star"checked></span>'
+    
    
     let puntajeEstrella = ""
    
     for(let i = 0; i < puntaje ; i++){
         puntajeEstrella += estrellaLlena
     }
-    for(let i = 0; i < 5-puntaje ; i++){
+    for(let i = 0; i < (5-puntaje) ; i++){
         puntajeEstrella += estrellaVacia
     }
     return puntajeEstrella
 }
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            product = resultObj.data;
-
-            let productNameHTML = document.getElementById("product-name");
-            let productDescriptionHTML = document.getElementById("product-description");
-            let productCountHTML = document.getElementById("product-count");
-           
-
-            productNameHTML.innerHTML = product.name;
-            productDescriptionHTML.innerHTML = product.description;
-            productCountHTML.innerHTML = product.soldCount;
-            
-
-            //Muestro las imagenes en forma de galería
-            showImages(product.images);
-        }
-    });
-});
-document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            comments = resultObj.data;
-
-            showComments(comments);
-            comment()
-        }
-    });
-});
-
-document.getElementById("enviar").addEventListener("click", comment)
-function comment(){
-        let user = sessionStorage.getItem("Usuario");
-        let addComment = document.getElementById("comment").value;
-        let commentToAppend = "";
-    if (addComment!= ""){    
-        
-            commentToAppend = `
-            <div class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">` + user + `</h5><p style="text-align: right"> ` + addComment + `</p> 
-                        <p> ` + mostrarEstrellas(comment.score) + `</p>
-                    </div>
-                    <p class="mb-1"> 01/02/2022</p>
-
-                </div>
-            </div>
-
-            `
-
-            document.getElementById("comments-section").innerHTML += commentToAppend;
+//Creamos la funcion para agregar una puntuacion en un nuevo comentario
+function agregarEstrellas (puntuacion){
+    if (puntuacion[0].checked) {
+        return 5;        
+    }else if (puntuacion[1].checked) {
+        return 4;
+    }else if (puntuacion[2].checked) {
+        return 3;
+    }else if (puntuacion[3].checked) {
+        return 2;
+    }else if (puntuacion[4].checked) {
+        return 1;        
     }
-};
+
+}
+
+
 
 
